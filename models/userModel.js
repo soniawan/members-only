55c1027698bd9c11ab73c2380b5c5693728b5737
soniawan <soniinozz9@gmail.com>
2026-05-11 14:16:41 +0700

    feat(user): create user model

---
+++ models/userModel.js
@@ -0,0 +1,27 @@
+import pool from "../db/pool.js";
+import db from "../db/pool.js";
+
+const insertUser = async (first_name, last_name, email, password) => {
+  await pool.query(
+    `INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)`,
+    [first_name, last_name, email, password],
+  );
+};
+
+const getUserById = async (id) => {
+  const { rows } = await pool.query(`SELECT * FROM users WHERE id = $1`, [id]);
+  return rows[0];
+};
+
+const getUserByEmail = async (email) => {
+  const { rows } = await pool.query(`SELECT * FROM users WHERE email = $1`, [
+    email,
+  ]);
+  return rows[0];
+};
+
+const updateToMember = async (id) => {
+  await pool.query(`UPDATE users SET isMember = true WHERE id = $1`, [id]);
+};
+
+export { insertUser, getUserById, getUserByEmail, updateToMember };
