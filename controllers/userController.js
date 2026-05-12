0212f8ab61ac2d2d9f227da220cec61d18f3721b
soniawan <soniinozz9@gmail.com>
2026-05-12 13:41:59 +0700

    feat(user): add user controller

---
+++ controllers/userController.js
@@ -0,0 +1,69 @@
+import { matchedData, validationResult } from "express-validator";
+import { validateUserPost } from "../validators/userValidation.js";
+import bcrypt from "bcryptjs";
+import userModel from "../models/userModel.js";
+import passport from "passport";
+
+const userSignUpGet = (req, res) => res.render("sign-up");
+
+const userSignUpPost = [
+  validateUserPost,
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).render("sign-up", {
+          title: "Sign up",
+          errors: errors.array(),
+          formData: req.body
+        });
+      }
+
+      const { firstName, lastName, email, password } = matchedData(req);
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await userModel.insertUser(firstName, lastName, email, hashedPassword);
+      res.redirect("/log-in");
+    } catch (err) {
+      next(err);
+    }
+  },
+];
+
+const userLogInGet = (req, res, next) => {
+  try {
+    const messages = req.session.messages || [];
+    req.session.messages = [];
+    res.render("log-in", { messages, formData: req.body });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const userLogInPost = passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/log-in",
+  failureMessage: true,
+});
+
+const userIndexGet = (req, res, next) => {
+  try {
+    res.render("index", {
+      user: req.user,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const userLogOutGet = (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+
+    req.session.destroy(() => {
+      res.clearCookie("connect.sid");
+      res.redirect("/");
+    })
+  });
+}
+
+export default { userSignUpGet, userSignUpPost, userLogInGet, userLogInPost, userLogOutGet, userIndexGet };
