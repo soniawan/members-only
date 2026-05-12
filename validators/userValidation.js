76c563fb67bda2d1bdf5421d3e5490aa50ea56f4
soniawan <soniinozz9@gmail.com>
2026-05-12 13:41:14 +0700

    feat: add user validation

---
+++ validators/userValidation.js
@@ -0,0 +1,36 @@
+import { body } from "express-validator";
+import userModel from "../models/userModel.js";
+
+const validateUserPost = [
+  body("firstName")
+    .trim()
+    .isLength({ min: 1, max: 100 })
+    .withMessage("First name must be between 1 and 100 characters"),
+  body("lastName")
+    .trim()
+    .isLength({ min: 1, max: 100 })
+    .withMessage("Last name must be between 1 and 100 characters"),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid email format")
+    .normalizeEmail({gmail_remove_dots: false})
+    .custom(async (value) => {
+      const user = await userModel.getUserByEmail(value);
+      if (user) throw new Error("Email already exists");
+      return true;
+    }),
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+  body("confirmPassword").custom((value, { req }) => {
+    if (value !== req.body.password) throw new Error("Password not match");
+    return true;
+  }),
+];
+
+export { validateUserPost };
