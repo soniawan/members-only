0ad10a5cc72f20771725e62e34107a4d19169a70
soniawan <soniinozz9@gmail.com>
2026-05-12 13:39:07 +0700

    refactor(user): update user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,9 @@
 import pool from "../db/pool.js";
-import db from "../db/pool.js";
 
-const insertUser = async (first_name, last_name, email, password) => {
+const insertUser = async (firstName, lastName, email, hashedPassword) => {
   await pool.query(
     `INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)`,
-    [first_name, last_name, email, password],
+    [firstName, lastName, email, hashedPassword],
   );
 };
 
@@ -24,4 +23,4 @@ const updateToMember = async (id) => {
   await pool.query(`UPDATE users SET isMember = TRUE WHERE id = $1`, [id]);
 };
 
-export { insertUser, getUserById, getUserByEmail, updateToMember };
+export default { insertUser, getUserById, getUserByEmail, updateToMember };
