6a22e5e5dce4c203b10871235dfc8fc3c120f9f8
soniawan <soniinozz9@gmail.com>
2026-05-12 13:46:56 +0700

    refactor: update app.js

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 import express from "express";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
+import session from "express-session";
+import "./config/passport.js"; 
+import passport from "passport";
+import userRouter from "./routes/userRouter.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello World"));
+// views
+app.set(
+  "views",
+  path.join(path.dirname(fileURLToPath(import.meta.url)), "views"),
+);
+app.set("view engine", "ejs");
+
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// route
+app.use("/", userRouter);
+
+// errors
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  const message = process.env.NODE_ENV === 'production'
+    ? "Sorry, there's a problem on our server"
+    : err.message;
+  res.status(500).send(message);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on localhost:${PORT}`);
