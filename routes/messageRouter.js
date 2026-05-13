ab8f57ec71e782b99888adfe393bce92b386cb5a
soniawan <soniinozz9@gmail.com>
2026-05-13 11:55:42 +0700

    feat(message): add delete message router

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -8,5 +8,6 @@ messageRouter.get("/messages", isAuth, messageController.messageCreateGet);
 messageRouter.post("/messages", isAuth, messageController.messageCreatePost);
 
 messageRouter.get("/", messageController.messageListGet)
+messageRouter.post("/messages/:messageId", isAuth, messageController.messageDeletePost)
 
 export default messageRouter;
