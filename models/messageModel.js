f197a39ddb2908490efcc528c98f5e29dbd29b7b
soniawan <soniinozz9@gmail.com>
2026-05-13 10:43:36 +0700

    feat(message): update get all messages model

--- models/messageModel.js
+++ models/messageModel.js
@@ -8,7 +8,13 @@ const insertMessage = async (userId, title, text, createdAt = new Date()) => {
 };
 
 const getAllMessages = async () => {
-  const { rows } = await pool.query("SELECT * FROM messages");
+  const { rows } = await pool.query(`
+    SELECT 
+      messages.*, users.first_name, users.last_name, users.is_admin 
+    FROM messages 
+    INNER JOIN users ON messages.user_id = users.id 
+    ORDER BY messages.created_at DESC
+  `);
   return rows;
 };
 
